71a0c936586efd940c0a166d53f0c7b303599a00
guoxingjun <guoxingjun@meituan.com>
2025-05-16 11:29:39 +0800

    fix: url last ch

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,9 @@ export const isNumber = (x: unknown): x is number => {
 
 export function joinUrl(...args: (string | number)[]): string {
   let url = String(args[0])
+  while (url[url.length - 1] === '/') {
+    url = url.slice(0, -1)
+  }
   for (let i = 1; i < args.length; i++) {
     let arg = args[i]
     if (isNumber(arg)) {
